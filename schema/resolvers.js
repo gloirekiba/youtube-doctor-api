905c0a413f73ff4268ab6d474c04468ae801edc8
Gloire Kiba <kibagloire1530@gmail.com>
2022-06-29 17:11:44 +0200

    🥅 catch error on video resolver

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -3,13 +3,15 @@ const ytdl = require("ytdl-core");
 const resolvers = {
   Query: {
     validateURL: (_parent, { url }) => ytdl.validateURL(url),
-    video: async (_parent, { url }) =>
-      await ytdl
+    video: async (_parent, { url }) => {
+      if (!ytdl.validateURL(url)) throw new Error("Invalid url");
+      return await ytdl
         .getInfo(url)
         .then(({ videoDetails }) => videoDetails)
         .catch((err) => {
           throw new Error(err);
-        }),
+        });
+    },
   },
 };
 
